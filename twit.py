9e9aee891abcd1ed7d593543e37e163605d1b963
xw <chromadrive.prime@gmail.com>
2020-02-02 21:19:49 -0800

    code consolidation

--- twit.py
+++ twit.py
@@ -1,8 +1,14 @@
 import twitter
 import json
 
-TEST_HANDLE = "twitter_handle"
+TEST_HANDLE = "abruptus9"
 KEYWORDS = ["commission", "comm", "slot", "badge", "request", "ych"]
+
+# TODO: Rearrange methods in a way that makes sense
+# TODO: Replace all print statements with return t/f
+# TODO: Detect when comms are closed
+# TODO: Time-sensitive opening posts & count closed if later closed
+# TODO: Consolidate data into one method
 
 def initialize_api():
     api = None
@@ -24,7 +30,7 @@ def find_commission_info_in_latest_tweets(api=None, screen_name=None, num_tweets
     for tweet in tweets:
         if tweet.text.startswith("@") or tweet.text.startswith("RT @"):
             continue
-        if "open" in tweet.text.lower() and [k for k in KEYWORDS if k in tweet.text.lower()]:
+        if are_commissions_open_from_text(tweet.text):
             print("[[OPEN " + tweet.created_at + "]]: \n" + tweet.text.replace("\n", " "))
         # if "full" in tweet.text.lower() and [k for k in KEYWORDS if k in tweet.text.lower()]:
         #     print("[CLOSED]: " + tweet.text)
@@ -33,10 +39,26 @@ def get_user_data(api=None, screen_name=None):
     user = api.GetUser(screen_name=screen_name)
     description = user.description
     name = user.name
-    print(screen_name, name, description)
+    consolidated_text = description + " | " + name
+    print(name)
+    if are_commissions_open_from_text(consolidated_text):
+        print(" >> Name/Bio says open")
 
+def are_commissions_open_from_text(text):
+    text = text.lower()
+    if any([k for k in KEYWORDS if k in text]):
+        # print(consolidated_text)
+        if "open" in text:
+            if not "not" in text.split("open")[0]:
+                return True
+    return False
+
+
+# Debugging    
 if __name__ == "__main__":
     api = initialize_api()
-    get_user_data(api=api, screen_name=TEST_HANDLE)
-    find_commission_info_in_latest_tweets(api=api, screen_name=TEST_HANDLE)
-    
+    for handle in ["abruptus9", "Allosaurex", "DragonJourney", "VaneEltin", "mervvin_art", "HigsbyTheDeer"]:
+        get_user_data(api=api, screen_name=handle)
+        find_commission_info_in_latest_tweets(api=api, screen_name=handle)
+        print("\n=======\n")
+
